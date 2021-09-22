b356bb3dde3e3fcc8fb096400a586ad608afdb6d
shyvesCCD <pedroochavess97@gmail.com>
2021-09-21 23:24:50 -0300

    Utilizando cargo.

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, Rust world!");
+    println!("Hello, world!");
 }
--- hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.d
+++
@@ -1,5 +0,0 @@
-/home/shyzera/github/shyzera/learning-Rust/hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.rmeta: src/main.rs
-
-/home/shyzera/github/shyzera/learning-Rust/hello_cargo/target/debug/deps/hello_cargo-47c1ef6b6aeceb58.d: src/main.rs
-
-src/main.rs:

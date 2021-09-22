396e2d239778f340b4814e54bc8ff3caa34fa42d
shyvesCCD <pedroochavess97@gmail.com>
2021-09-21 23:15:00 -0300

    Apredendo sobre o cargo.

---
+++ hello_cargo/target/debug/deps/hello_cargo-bc40d0ab10576a3d.d
@@ -0,0 +1,5 @@
+/home/shyzera/github/shyzera/learning-Rust/hello_cargo/target/debug/deps/hello_cargo-bc40d0ab10576a3d.rmeta: src/main.rs
+
+/home/shyzera/github/shyzera/learning-Rust/hello_cargo/target/debug/deps/hello_cargo-bc40d0ab10576a3d.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/shyzera/github/shyzera/learning-Rust/hello_cargo/target/debug/hello_cargo: /home/shyzera/github/shyzera/learning-Rust/hello_cargo/src/main.rs
